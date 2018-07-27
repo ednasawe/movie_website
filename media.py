de1faf0f1f82cf232a1de61da3e99f6e9366428d
Edna <cedna198@gmail.com>
2018-07-27 13:55:38 +0300

    Changes on the media python file

--- media.py
+++ media.py
@@ -1,12 +1,14 @@
 import webbrowser
 
 class Movie():
+    """ This function is used to expalin what the class Movie() will be doing """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    """ This function is used to command the browser to open a specified movie trailer """
     def show_trailer(self):
          webbrowser.open(self.trailer_youtube_url)
          
